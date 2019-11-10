85aea4bc973854e2c7aebc7e53e9b78315024e8e
Marcus Hellberg <marcus@vaadin.com>
2019-11-09 21:10:33 -0800

    add spring security and login view

--- src/main/java/com/vaadin/tutorial/spring/ui/view/MainView.java
+++ src/main/java/com/vaadin/tutorial/spring/ui/view/MainView.java
@@ -3,7 +3,12 @@ package com.vaadin.tutorial.spring.ui.view;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.router.RouterLink;
@@ -12,16 +17,24 @@ import com.vaadin.flow.theme.lumo.Lumo;
 import com.vaadin.tutorial.spring.ui.view.dashboard.DashboardView;
 import com.vaadin.tutorial.spring.ui.view.list.ListView;
 
-@CssImport("./styles/styles.css")
 //@PWA(name = "VaadinCRM", shortName = "VaadinCRM")
+//@Theme(value = Lumo.class, variant = Lumo.DARK)
+@CssImport("./styles/styles.css")
 @Viewport("width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes, viewport-fit=cover")
-@Theme(value = Lumo.class, variant = Lumo.DARK)
 public class MainView extends AppLayout {
 
   public MainView() {
     H1 logo = new H1("Vaadin CRM");
     logo.addClassName("logo");
-    addToNavbar(new DrawerToggle(), logo);
+    Anchor logout = new Anchor("/logout", "Log out");
+
+    HorizontalLayout layout = new HorizontalLayout(new DrawerToggle(), logo, logout);
+    layout.addClassName("header");
+    layout.expand(logo);
+    layout.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+    layout.setWidth("100%");
+
+    addToNavbar(layout);
     addToDrawer(new VerticalLayout(
         new RouterLink("List", ListView.class),
         new RouterLink("Dashboard", DashboardView.class)));
---
+++ src/main/java/com/vaadin/tutorial/spring/ui/view/login/LoginView.java
@@ -0,0 +1,37 @@
+package com.vaadin.tutorial.spring.ui.view.login;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import java.util.Collections;
+
+@Route(value = LoginView.ROUTE)
+@PageTitle("Login")
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+	public static final String ROUTE = "login";
+
+	private LoginForm login = new LoginForm();
+
+	public LoginView(){
+		setSizeFull();
+		setAlignItems(Alignment.CENTER);
+		setJustifyContentMode(JustifyContentMode.CENTER);
+		login.setAction("login");
+
+		add(new H1("Vaadin CRM"), login);
+	}
+
+	@Override
+	public void beforeEnter(BeforeEnterEvent event) {
+		// inform the user about an authentication error
+		if(!event.getLocation().getQueryParameters().getParameters().getOrDefault("error", Collections.emptyList()).isEmpty()) {
+			login.setError(true);
+		}
+	}
+}
